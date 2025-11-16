fb8e6620557c3750f1b9ae9e2c1da0d39b7b501e
anonymous360-c <affanalvi360@gmail.com>
2025-11-17 00:09:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    let nav = document.getElementById("nav-links");
+    nav.style.display = (nav.style.display === "block") ? "none" : "block";
+}
+
+
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
